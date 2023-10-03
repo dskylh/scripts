0cef843353703cdfa86012588a031243becc8a79
Taha Ugurcan Ongel <tahagurcanoffical@gmail.com>
2023-10-03 21:48:08 +0300

    update stream.sh

---
+++ stream.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/taha/Downloads/KeyOverlay-ubuntu-latest/
+/home/taha/Downloads/KeyOverlay-ubuntu-latest/KeyOverlay &
+cd /home/taha/Downloads/gosumemory_linux_amd64/
+source /home/taha/Downloads/gosumemory_linux_amd64/ppcount.sh 
